9775e22bc542acd627ddc4961d9d86ab33676762
dalowa <david.developer.work@gmail.com>
2023-08-02 23:44:54 -0500

    basic about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function page() {
-  return <div>page</div>;
+export default function About() {
+  return (
+    <main>
+      <h2>This is the about page</h2>
+      <p>This is supposed to be a long explanation</p>
+    </main>
+  );
 }
