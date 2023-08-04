035b9bde8bec0ea3586f83dc0be88657bf637c94
dalowa <david.developer.work@gmail.com>
2023-08-04 00:12:55 -0500

    Fetch data from TMDB API in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
-import Image from "next/image";
+import Results from "@/components/Results";
 
-export default function Home() {
-  return <main>Home</main>;
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }: { searchParams: any }) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
+  }
+
+  const data = await res.json();
+
+  const results = data.results;
+
+  /* console.log(results); */
+
+  return (
+    <main>
+      <h1>Aqui</h1>
+      <Results results={results} />
+    </main>
+  );
 }
